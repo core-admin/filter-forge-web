6f462af041aa721385d4d9062da3b013a68c8ee8
xuke <xk1242425209@163.com>
2025-03-04 23:27:02 +0800

    修改主题色

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 
 export default function Home() {
@@ -34,6 +35,8 @@ export default function Home() {
           >
             Read our docs
           </a>
+
+          <Button>嘿嘿</Button>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
